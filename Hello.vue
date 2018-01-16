ca646ab8a97e7e49b71ffe2f763d2139c7398970
Ran Guin <ran.guin@gmail.com>
2018-01-15 19:31:40 -0800

    add cart & tidy home page

--- Hello.vue
+++ Hello.vue
@@ -1,76 +1,122 @@
 <template lang='pug'>
   div.page
-    div.header
-      PublicHeader
-    div.subheader.overlay
-      CustomMenu(:links='links' type='url')
-    div.body.container
-      img.bg
-      div.navbar-right
-        div.cart
-          h3 Cart
-            br
-            b ({{items}} items)
-          ul
-            li(v-for="item in cart")
-              b {{item.name}} [ {{item.amount}} x {{item.qty}} ] = {{item.amount*item.qty}}
-          form(action="/process-payment" method="POST") 
-            button.btn.btn-primary(:disabled="!items") Checkout
-              <!-- stripe-checkout(stripe-key="my-stripe-key" product="product(tea.name, tea.description, tea.price100g)") -->
+    div.body
       div.overlay
-        h2 T4U
-        h3 Black Teas
-        ul
-          li(v-for="tea in teas")
-            div(v-if="tea.name === ''")
-              b {{tea.variety}} Teas:
-            div(v-else)
-              div(v-if="tea.name === tea.variety")
-               b {{tea.variety}}
-              h4
-                button.btn.btn-primary.btn-xs(@click.prevent="add2cart(tea.id, tea.name, tea.description, tea.price100g, 1)") +
-                b &nbsp; &nbsp; 
-                span(data-toggle='tooltip' :title="tea.description") {{tea.name}} [ ${{tea.price100g}} ] 
+        div.navbar-left
+          CustomMenu(direction='horizontal' :links='links' type='function' :onClick="chooseType" :options='options')
+        div(style='width:100%')
+          div.navbar-right
+            button(@click.prevent='uploadTeas') Upload Teas
+            span &nbsp; &nbsp;
+            span &nbsp; &nbsp;
+            Modal(id='search-modal' type='search' :options="search_options" :picked="selected" :onDelete='deleteItem')
+          table.table
+            tr
+              td(colspan=2)
+                hr
+            tr
+              td.teas
+                Teas(:teas="teas" :page="page" :add="add2cart" :type="active_type")
+              td
+                Cart(:cart="cart" :add="add2cart")
+
+
     div.footer
       PublicFooter
 
 </template>
 
 <script>
-// import config from '@/config.js'
 import PublicHeader from './../PublicHeader'
 import PublicFooter from './../PublicFooter'
+import Teas from './Teas'
 import CustomMenu from './../Standard/Menu'
+import Cart from './../Standard/Cart'
+import Modal from './../Standard/Modal'
+
+import axios from 'axios'
+
+import 'vue-awesome/icons/shopping-cart'
+import 'vue-awesome/icons/close'
+import 'vue-awesome/icons/home'
+import 'vue-awesome/icons/search'
 
 // import { StripeCheckout } from 'vue-stripe'
-
-var links = ['Main']
 
 export default {
   name: 'hello',
   components: {
     PublicHeader,
     CustomMenu,
-    PublicFooter
+    PublicFooter,
+    Teas,
+    Cart,
+    Modal
   },
   data () {
     return {
       msg: 'Welcome to My Vue.js App',
       // ################## Add Link List Below for Testing ######################
-      links: links,
+      // links: ['Teas', 'Ceramics', 'About Us'],
 
+      showCart: false,
       // Test Data ###
       cart: {},
+      pages: ['Black', 'Green', 'White', 'Red'],
+      page: 'Black',
       varieties: ['Earl Grey', 'Pu Ehr'],
       teas: [
-        {id: 1, name: '', variety: 'Earl Grey', description: 'Classic Black Tea with Bergamont & Corn Flowers', price100g: '12.00', price200g: '22.00'},
-        {id: 2, name: 'Earl Grey Cream', variety: 'Earl Grey', description: 'Classic Black Tea with Bergamont & Corn Flowers', price100g: '12.00', price200g: '22.00'},
-        {id: 3, name: 'Lady Earl Grey', variety: 'Earl Grey', description: 'Classic Earl Grey with Vanilla', price100g: '12.00', price200g: '22.00'},
-        {id: 4, name: 'Pu Ehr', variety: 'Pu Ehr', description: 'Classic Pu Ehr', price100g: '45.00', price200g: '82.00'}
-      ]
+        {id: 1, name: '', type: 'Black', variety: 'Earl Grey', description: 'Classic Black Tea with Bergamont & Corn Flowers', price100g: '12.00', price200g: '22.00', qty: 1, amount: '12.00'},
+        {id: 2, name: 'Earl Grey Cream', type: 'Black', variety: 'Earl Grey', description: 'Classic Black Tea with Bergamont & Corn Flowers', price100g: '12.00', price200g: '22.00', qty: 1, amount: '12.00'},
+        {id: 3, name: 'Lady Earl Grey', type: 'Black', variety: 'Earl Grey', description: 'Classic Earl Grey with Vanilla', price100g: '12.00', price200g: '22.00', qty: 1, amount: '12.00'},
+        {id: 4, name: 'Pu Ehr', type: 'Black', variety: 'Pu Ehr', description: 'Classic Pu Ehr', price100g: '45.00', price200g: '82.00', qty: 1, amount: '12.00'},
+        {id: 5, name: 'Oolong', type: 'Green', variety: 'Oolong', description: 'Classic Oolong', price100g: '55.00', price200g: '93.00', qty: 1, amount: '12.00'},
+        {id: 6, name: 'Rooibos - Honeybush', type: 'Rooibos', variety: 'Rooibos', description: 'Classic Rooibos - Honeybush', price100g: '55.00', price200g: '93.00', qty: 1, amount: '12.00'}
+      ],
+      links: [{Black: 'Black', Green: 'Green', Red: 'Rooibos'}],
+      options: {default: 'Black', colour: 'blue', hoverColour: 'red'},
+      active_type: 'Black',
+
+      // Modal Options
+      info_options: {
+        access_type: 'append',
+        table: 'tea',
+        close: 'Cancel',
+        toggle: false
+        // onSave: this.saveImmunization
+      },
+      search_options: {
+        type: 'search',
+        title: 'Tea Varieties',
+        openIcon: 'search',
+        closeButton: 'Close Scheduler',
+        search: {
+          scope: 'selectTeas',
+          model: 'selectTeas',
+          method: 'get',
+          url: '/search',
+          prompt: 'Search Teas',
+          field: 'name',
+          search_fields: ['name', 'variety', 'type'],
+          show_fields: ['name', 'description', 'variety', 'type'],
+          // onPick: this.addCoverage,
+          multiSelect: true,
+          target: 'selectTeas',
+          picked: this.selectedTeas,
+          stored: 'selectTeas'
+        },
+        data: {
+          onDelete: this.deleteItem,
+          stored: 'coverage'
+        }
+      },
+      selected: []
     }
   },
   computed: {
+    selectedTeas: function () {
+      return [{name: 'Selected', description: 'fill desc', variety: 'tea v'}]
+    },
     product: function (name, desc, price) {
       return { name: name, description: desc, amount: price }
     },
@@ -85,17 +131,73 @@ export default {
     }
   },
   methods: {
-    add2cart (id, name, desc, amount, qty) {
-      if (this.cart[id]) {
-        console.log('add another ' + id)
-        var count = this.cart[id].qty + 1
-        // var item = {id: id, name: name, description: desc, amount: amount, qty: qty}
-        this.$set(this.cart[id], 'qty', count)
+    uploadTeas: function () {
+      var url = 'http://127.0.0.1:3333/upload'
+      var data = { name: 'Earl Gray', type: 'Black', price: '15.99' }
+      console.log(url + ' sent: ' + JSON.stringify(data))
+
+      axios.post(url, data)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        console.log('axios response: ' + JSON.stringify(response.data))
+      })
+      .catch(e => {
+        console.log('error uploading Teas...')
+        console.log(e)
+      })
+    },
+    teaInfo: function (record) {
+      console.log('retrieve more schedule info from record: ' + JSON.stringify(record))
+
+      var data = [
+        {'tea': 'Earl Grey', details: 'fill in details..'}
+      ]
+
+      this.$store.dispatch('setModalData', data)
+      this.$store.getters.toggleModal('info-modal')
+    },
+
+    toggleSearch: function (record) {
+      this.$store.getters.toggleModal('info-modal')
+    },
+
+    deleteItem: function (index, key) {
+      if (!key) { key = 'coverage' }
+      console.log('DYNO: delete ' + key + ' item: ' + index)
+      // this.$store.dispatch('deleteHashItem', {key: key, index: index})
+
+      this.$store.commit('removeHashItem', {key: key, index: index})
+    },
+    add2cart (list) {
+      console.log('add item to cart: ' + JSON.stringify(list))
+
+      var items = []
+      if (list.constructor === Object) {
+        items.push(list)
+      } else if (list.constructor === Array) {
+        items = list
       } else {
-        var item = {id: id, name: name, description: desc, amount: amount, qty: qty}
-        console.log('added ' + JSON.stringify(item))
-        this.$set(this.cart, id, item)
+        console.log('can only add hash or array to cart....')
       }
+
+      for (var i = 0; i < items.length; i++) {
+        var item = items[0]
+        var id = item.id
+        if (this.cart[id]) {
+          var count = this.cart[id].qty || 0
+          var add = item.qty || 1
+          count += add
+          this.$set(this.cart[id], 'qty', count)
+        } else {
+          var qty = item.qty || 1
+          var newItem = {id: id, name: item.name, description: item.desc, amount: item.amount, qty: qty}
+          this.$set(this.cart, id, newItem)
+        }
+      }
+    },
+    chooseType (type) {
+      console.log('activate ' + type)
+      this.active_type = type
     }
   }
 }
@@ -105,16 +207,24 @@ export default {
 <style scoped lang="sass?outputStyle=expanded">
 
 /*** Customize Header / Footer Settings: ***/
-$header-height: 80px;
-$header-background-colour: #cfc;
+$header-height: 0px;
+$header-background-colour: transparent;
 $header-padding: 10px 30px;
 
-$subheader-height: 50px;
+$subheader-height: 40px;
 $subheader-background-colour: transparent;
 
 $footer-height: 90px;
-$footer-background-colour: #ffc;
+$footer-background-colour: transparent;
 
+/*.page { 
+  background: url("/static/images/teapour.jpeg") no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+*/
 .page {
   /*margin-top: -20px;*/
   height: 100%;
@@ -146,17 +256,48 @@ $footer-background-colour: #ffc;
 }
 
 .body {
-  min-height: calc(100vh - #{$header-height} - #{$footer-height} - #{$subheader-height});
+  min-height: calc(100vh - #{$header-height} - #{$footer-height});
   color: white;
   z-index: 10000;
+  padding: 20px;
 }
 
-img.bg {
+.teas {
+  vertical-align: top;
+  width: 100%;
+}
+
+.cart {
+  /*position: relative;*/
+}
+/*.table {
+  position: relative;
+  margin-left: -1%;
+}*/
+
+/*.overlay {
+  position: relative;
+  margin-left: -10%;
+}
+*/
+
+.relative {
+  position: relative;
+}
+
+@media screen and (max-width: 512px) { /* Specific to this particular image */
+  img.bg {
+    display: none;
+  }
+}
+
+.page {
   z-index: -1;
   /* Set rules to fill background */
   min-height: 100%;
   /*min-width: 1024px;*/
-  background-image: url("/static/images/teapour.jpeg");
+
+  background-image: url("/static/images/tealeaves.jpeg");
 /* Center and scale the image nicely */
   background-position: center;
   background-repeat: no-repeat;
@@ -177,6 +318,7 @@ img.bg {
   left: 0;
 }
 
+
 @media screen and (max-width: 1024px) { /* Specific to this particular image */
   img.bg {
     left: 50%;
@@ -185,21 +327,14 @@ img.bg {
 }
 
 .overlay {
-  position: absolute;
+  /*position: absolute;*/
+  /*width: 100%;*/
 /*  top: 50%;
   left: 50%;*/
   /*transform: translate(-50%, -50%);*/
-  color: white;
+  color: #333;
   /*text-align: center;*/
-}
-
-.cart {
-  position: fixed;
-  right: 20px;
-  background-color: #cfc;
-  padding: 10px;
-  border: 1px solid black;
-  /*margin: 10px;*/
+  z-index:1000;
 }
 
 h1, h2 {
